b6d680cd32f3fb9afd19f81123827da6d89a756b
Hussain <hussain.nauman.92@gmail.com>
2025-10-22 10:12:31 +0200

    fixed relative path issue

--- js/config.js
+++ js/config.js
@@ -1,12 +1,5 @@
 // js/config.js
 
-// Define the base path for your website.
-// Examples:
-//   - For GitHub Pages: '/Lebaz-Lernstation/'
-//   - For deploying to the root of your own domain: '/'
-//   - For deploying to a subdirectory 'my-app' on your own domain: '/my-app/'
-const BASE_PATH = '/Lebaz-Lernstation/';
-
 // Define the icon styling method for the header navigation.
 // true = Use CSS grayscale filter (only need color icons).
 // false = Use separate gray/green image files (need both color and gray icons).
